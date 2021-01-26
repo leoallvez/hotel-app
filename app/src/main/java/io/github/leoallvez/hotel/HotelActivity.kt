efdaa256f6eca630e7be7876b4bc2a832dac958c
Leonardo <leoallvez@hotmail.com>
2021-01-25 21:46:47 -0300

    actionbar

--- app/src/main/java/io/github/leoallvez/hotel/HotelActivity.kt
+++ app/src/main/java/io/github/leoallvez/hotel/HotelActivity.kt
@@ -2,19 +2,101 @@ package io.github.leoallvez.hotel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.PersistableBundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.SearchView
+import kotlinx.android.synthetic.*
 
-class HotelActivity : AppCompatActivity(), HotelListFragment.OnHotelClickListener {
+class HotelActivity : AppCompatActivity(),
+        HotelListFragment.OnHotelClickListener,
+        SearchView.OnQueryTextListener,
+        MenuItem.OnActionExpandListener {
+
+    private var lastSearchTerm: String = ""
+    private var searchView: SearchView? = null
+    private val listFragment: HotelListFragment by lazy {
+        supportFragmentManager.findFragmentById(R.id.fragmentList) as HotelListFragment
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hotel)
     }
 
-    override fun onHotelClick(hotel: Hotel) {
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(EXTRA_SEARCH_TERM, lastSearchTerm)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        lastSearchTerm = savedInstanceState?.getString(EXTRA_SEARCH_TERM) ?: ""
+    }
+
+    override fun onHotelClick(hotel: Hotel) = if(isTablet()) {
+        showDetailsFragment(hotel.id)
+    } else {
         showDetailsActivity(hotel.id)
+    }
+
+    private fun showDetailsFragment(hotelId: Long) {
+        searchView?.setOnQueryTextListener(null)
+        val fragment = HotelDetailsFragment.newInstance(hotelId)
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.details, fragment, HotelDetailsFragment.TAG_DETAILS)
+            .commit()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.hotel, menu)
+        val searchItem = menu?.findItem(R.id.action_search)
+        searchItem?.setOnActionExpandListener(this)
+        searchView = searchItem?.actionView as SearchView
+        searchView?.queryHint = getString(R.string.hint_search)
+        searchView?.setOnQueryTextListener(this)
+
+        if(lastSearchTerm.isEmpty()) {
+            Handler().post {
+                val query = lastSearchTerm
+                searchItem.expandActionView()
+                searchView?.setQuery(query, true)
+                searchView?.clearFocus()
+            }
+        }
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onQueryTextSubmit(query: String?) = true
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        lastSearchTerm = newText ?: ""
+        listFragment.search(lastSearchTerm)
+        return true
     }
 
     private fun showDetailsActivity(hotelId: Long) {
         HotelDetailsActivity.open(this, hotelId)
+    }
+
+    override fun onMenuItemActionExpand(item: MenuItem?) = true
+
+    override fun onMenuItemActionCollapse(item: MenuItem?): Boolean {
+        lastSearchTerm = ""
+        listFragment.clearSearch()
+        return true
+    }
+
+    private fun isTablet() = findViewById<View>(R.id.details) != null
+
+    companion object {
+        const val EXTRA_SEARCH_TERM = "lastSearch"
     }
 }
